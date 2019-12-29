4efc315da307cf583685de07c657f4e3b9ecebbe
Androz2091 <androz2091@gmail.com>
2019-12-29 09:56:21 +0100

    Add base cache save

--- helpers/autoNotif.js
+++ helpers/autoNotif.js
@@ -2,6 +2,11 @@ const { CronJob } = require("cron");
 const getMoyennes = require("../pronote/getMoyennes");
 const InstaUser = require("../instagram/InstaUser");
 const logger = require("../helpers/logger");
+const beautify = require("json-beautify");
+const fs = require("fs");
+const reload = require("require-reload")(require);
+const { promisify } = require("util");
+const write = promisify(fs.writeFile);
 
 const asyncForEach = async (array, callback) => {
   for (let index = 0; index < array.length; index++) {
@@ -9,8 +14,8 @@ const asyncForEach = async (array, callback) => {
   }
 }
 
-module.exports.init = (ig) => {
-    new CronJob("55 16 15 * * *", async () => {
+module.exports.init = async (ig) => {
+    const autoNotif = async () => {
         logger.log("Auto-check...");
         // Load all credentials
         let credentials = require("../credentials.json");
@@ -25,25 +30,47 @@ module.exports.init = (ig) => {
             logger.log("Cache retrieved. (session="+cred.username+")");
             let moyennes = await getMoyennes(cred).catch(console.error);
             if(!moyennes) return;
+            if(!credCache){
+                credCache = {
+                    insta: cred.insta,
+                    lastMoyenne: moyennes.moyNormale
+                };
+                cache.push(credCache);
+                await write("./cache.json", beautify(cache, null, 2, 100), "utf-8");
+                reload("../cache.json");
+                logger.log("Base cache saved. (session="+cred.username+")");
+            }
             logger.log("Averages retrieved. (session="+cred.username+")");
             if(credCache.lastMoyenne !== moyennes.moyPluri){
-                let finalString = `Votre moyenne est passée de ${credCache.lastMoyenne} à ${moyennes.moyPluri}.`;
+                let finalString = `🔔PronoteBot [process.all]\n\nVotre moyenne est passée de ${credCache.lastMoyenne} à ${moyennes.moyPluri}.\n`;
                 logger.log("Differences detected. (session="+cred.username+")");
                 if(moyennes.diff.length > 0){
                     logger.log("Subject differences detected. (session="+cred.username+")");
-                    finalString += `Les matières suivantes ont été modifiées :\n\n`;
+                    finalString += `Liste matières modifiées (${moyennes.diff.length}) :\n\n`;
                     moyennes.diff.forEach((m) => {
-                        finalString += m.nom+" | "+m.ancienneMoyenne+" => "+m.nouvelleMoyenne
+                        finalString += m.matiere+" | "+m.ancienneMoyenne+" => "+m.nouvelleMoyenne+"\n"+
+                        ((parseFloat(m.nouvelleMoyenne.replace(",", "."))-parseFloat(m.ancienneMoyenne.replace(",", ".")) > 0) ?
+                        "Augmentation de "+String((parseFloat(m.nouvelleMoyenne.replace(",", "."))-parseFloat(m.ancienneMoyenne.replace(",", "."))).toFixed(2)).replace(".", ",")+" point(s)." :
+                        "Baisse de "+String((parseFloat(m.nouvelleMoyenne.replace(",", "."))-parseFloat(m.ancienneMoyenne.replace(",", "."))).toFixed(2)).replace(".", ",")+" point(s).")
                     });
                 } else {
                     logger.log("No subject differences detected. (session="+cred.username+")");
                 }
                 let user = new InstaUser(cred.insta, ig);
                 user.send(finalString);
+                cache.find((c) => c.insta === cred.insta).lastMoyenne = moyennes.moyPluri;
+                await write("./cache.json", beautify(cache, null, 2, 100), "utf-8");
+                logger.log("New cache saved. (session="+cred.username+")");
+                logger.log("-----------END------- (session="+cred.username+")");
+                return;
             } else {
                 logger.log("No differences. (session="+cred.username+")");
+                logger.log("-----------END------- (session="+cred.username+")");
+                return;
             }
         });
-        
-    }, null, true, "Europe/Paris");
+        logger.log("-----------END------- (session=ALL)");
+    };
+    autoNotif();
+    new CronJob("55 16 15 * * *", autoNotif, null, true, "Europe/Paris");
 };
