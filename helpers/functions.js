e6b029b2934f03d34c1ddb089bf4b284d55978a2
Androz2091 <androz2091@gmail.com>
2020-01-30 17:59:05 +0100

    Ajout d'une fonction getMenuNom()

--- helpers/functions.js
+++ helpers/functions.js
@@ -15,10 +15,59 @@ const asyncForEach = async (array, callback) => {
     }
 };
 
-const delay = ms => new Promise(res => setTimeout(res, ms));
+const delay = ms => {
+    return new Promise(res => {
+        setTimeout(res, ms);
+    });
+};
+
+const getLundi = date => {
+    let day = date.getDay();
+    let diff = date.getDate() - day + (day == 0 ? -6 : 1);
+    return new Date(date.setDate(diff));
+};
+
+const getVendredi = date => {
+    let lundi = new Date(date);
+    lundi.setDate(lundi.getDate() + 4);
+    return lundi;
+};
+
+const getMois = date => {
+    return [
+        "janvier",
+        "février",
+        "mars",
+        "avril",
+        "mai",
+        "juin",
+        "juillet",
+        "août",
+        "septembre",
+        "octobre",
+        "novembre",
+        "décembre"
+    ][date.getMonth()];
+};
+
+const getMenuNom = () => {
+    let dateWeek = new Date();
+    // Si c'est vendredi, samedi ou dimanche
+    if (
+        new Date().getDay() === 5 ||
+        new Date().getDay() === 6 ||
+        new Date().getDay() === 7
+    ) {
+        dateWeek.setDate(dateWeek.getDate() + 4);
+    }
+    return `${getLundi(dateWeek).getDate()}-au-${getVendredi(
+        dateWeek
+    ).getDate()}-${getMois(dateWeek)}-${dateWeek.getFullYear()}`;
+};
 
 module.exports = {
     formatMatiere,
     asyncForEach,
-    delay
+    delay,
+    getMenuNom
 };
