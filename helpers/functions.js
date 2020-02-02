1ac43302293d06b33b60bdb2c20be4452a01735f
Androz2091 <androz2091@gmail.com>
2020-02-02 19:33:42 +0100

    Ajout d'un pad pour le menu

--- helpers/functions.js
+++ helpers/functions.js
@@ -53,15 +53,18 @@ const getMois = date => {
 const getMenuNom = () => {
     let dateWeek = new Date();
     // Si c'est samedi ou dimanche
-    if (
-        new Date().getDay() === 6 ||
-        new Date().getDay() === 0
-    ) {
+    if (new Date().getDay() === 6 || new Date().getDay() === 0) {
         dateWeek.setDate(dateWeek.getDate() + 4);
     }
-    return `${getLundi(dateWeek).getDate()}-au-${getVendredi(
-        dateWeek
-    ).getDate()}-${getMois(dateWeek)}-${dateWeek.getFullYear()}`;
+    return `${
+        String(getLundi(dateWeek).getDate()).length === 1
+            ? `0${getLundi(dateWeek).getDate()}`
+            : getLundi(dateWeek).getDate()
+    }-au-${
+        String(getVendredi(dateWeek).getDate()).length === 1
+            ? `0${getVendredi(dateWeek).getDate()}}`
+            : getVendredi(dateWeek).getDate()
+    }-${getMois(dateWeek)}-${dateWeek.getFullYear()}`;
 };
 
 module.exports = {
